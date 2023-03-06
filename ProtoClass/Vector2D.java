d308fdd9449e35f89563861d637620b27cd1d007
Din <hurshid.17.97@mail.ru>
2023-03-06 15:30:53 +0500

    ProtoClass

---
+++ ProtoClass/Vector2D.java
@@ -0,0 +1,17 @@
+package ProtoClass;
+
+import java.util.ArrayList;
+
+public class Vector2D {
+    public int x;
+    public int y;
+    public Vector2D(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    protected Double getDistance(Vector2D enemy){
+        return Math.sqrt((x - enemy.x)^2 + (y - enemy.y)^2);
+    }
+}
+
